555e353c4be06bc940176bd061fc8317c37f20aa
Yash Mahalwal <yashmahalwal@gmail.com>
2024-04-10 21:23:40 +0530

    chore(unoptimised): added js-docs

--- src/routes/unoptimised/unoptimised.tsx
+++ src/routes/unoptimised/unoptimised.tsx
@@ -13,24 +13,35 @@ import {
 } from "@nextui-org/react";
 import { Line } from "react-chartjs-2";
 
+/**
+ * Component for displaying unoptimized stock monitoring.
+ * @returns JSX element
+ */
 export function Unoptimised() {
+  // State to store list of stock events
   const [stockEventList, setStockEventList] = useState<StockEvent[]>([]);
+  // State to store list of running average of prices
   const [averagePrices, setAveragePrices] = useState<number[]>([]);
+  // Ref for the table element
   const tableRef = useRef<HTMLDivElement>(null);
 
+  // Custom hook to monitor stocks
   const { observe, unobserve, isWatching } = useMonitorStocks((event) => {
+    // Add new stock event to the list
     setStockEventList((old) => [...old, event]);
   });
 
+  // Side effect of adding a stock event
   useEffect(() => {
     if (stockEventList.length) {
+      // Calculate average prices
       const event = stockEventList.at(-1)!;
       const newTotal =
         stockEventList.reduce((old, current) => old + current.price, 0) +
         event.price;
       const newAverage = newTotal / (stockEventList.length + 1);
       setAveragePrices((old) => [...old, newAverage]);
-
+      // Scroll to bottom of table
       const scrollContainer = tableRef.current?.parentElement;
       scrollContainer?.scrollTo({
         top: scrollContainer.scrollHeight,
@@ -39,14 +50,18 @@ export function Unoptimised() {
     }
   }, [stockEventList]);
 
+  // Handler to reset stock event list and average prices
   const handleReset = () => {
     setStockEventList([]);
     setAveragePrices([]);
   };
+
+  // Handler to toggle stock monitoring
   const handleWatchToggle = () => {
     isWatching ? unobserve() : observe();
   };
 
+  // Columns for the table
   const tableColumns = [
     {
       key: "name",
@@ -58,11 +73,14 @@ export function Unoptimised() {
     },
   ];
 
+  // Last 50 average prices
   const last50Prices = averagePrices.slice(-50);
+  // Labels for the chart
   const labels = last50Prices.map(
     (_, i) => i + Math.max(averagePrices.length - 50, 0)
   );
 
+  // Data for the chart
   const chartData = {
     labels,
     datasets: [
@@ -82,13 +100,16 @@ export function Unoptimised() {
       <h1 className="text-xl text-center">Unoptimised Stock Monitoring</h1>
       <article className="flex flex-col align-middle mt-4">
         <div className="flex gap-2 justify-center">
+          {/* Button to toggle stock monitoring */}
           <Button color="primary" variant="solid" onClick={handleWatchToggle}>
             {isWatching ? "Unobserve" : "Observe"}
           </Button>
+          {/* Button to reset stock event list */}
           <Button color="primary" variant="bordered" onClick={handleReset}>
             Reset
           </Button>
         </div>
+        {/* Line chart to display average prices */}
         <section className="mt-6">
           <Line
             options={{
@@ -97,6 +118,7 @@ export function Unoptimised() {
             data={chartData}
           />
         </section>
+        {/* Table to display stock events */}
         <section className="mt-6">
           <Table
             ref={tableRef}
