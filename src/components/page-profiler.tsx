0441cc7c41db516c3d14aa98326957981f102937
Yash Mahalwal <yashmahalwal@gmail.com>
2024-04-10 21:11:55 +0530

    feat(profiler): added plots

--- src/components/page-profiler.tsx
+++ src/components/page-profiler.tsx
@@ -3,16 +3,14 @@ import {
   ProfilerProps,
   PropsWithChildren,
   useEffect,
+  useMemo,
   useRef,
   useState,
 } from "react";
 import { Button, Code } from "@nextui-org/react";
 import { useStableCallback } from "../hooks/use-stable-callback";
 import { useMatch } from "react-router-dom";
-
-export type PageProfilerProps = {
-  durationMs?: number;
-};
+import { Line } from "react-chartjs-2";
 
 type ProfileEvent = {
   render: number;
@@ -31,6 +29,83 @@ const defaultProfiledData: ProfiledData = {
   count: 0,
 };
 
+type LinePlotProps = {
+  list: ProfiledData[];
+  label: string;
+  selector: (data: ProfiledData) => number;
+  color: string;
+};
+function LinePlot({ list, label, selector, color }: LinePlotProps) {
+  const config = useMemo(
+    () => ({
+      labels: list.map((_, i) => i),
+      datasets: [
+        {
+          label,
+          data: list.map((e) => selector(e)),
+          fill: false,
+          borderColor: color,
+          tension: 0.4,
+        },
+      ],
+    }),
+    [color, label, list, selector]
+  );
+
+  return (
+    <div className="mt-2">
+      <Line options={plotOptions} data={config} />
+      <p className="text-center text-xs text-slate-500">{label}</p>
+    </div>
+  );
+}
+
+type ProfilePlotProps = {
+  list: ProfiledData[];
+};
+
+const plotOptions = { animation: false } as const;
+
+function ProfilePlot({ list }: ProfilePlotProps) {
+  const selectRenderDuration = useStableCallback(
+    (data: ProfiledData) => data.averageRenderDuration
+  );
+  const selectRenderCount = useStableCallback(
+    (data: ProfiledData) => data.count
+  );
+  const selectTimeSpent = useStableCallback(
+    (data: ProfiledData) =>
+      data.count * (data.averageRenderDuration + data.averageCommitDuation)
+  );
+
+  return (
+    <>
+      <LinePlot
+        color="rgba(252,127,3)"
+        label="Render Count"
+        list={list}
+        selector={selectRenderCount}
+      />
+      <LinePlot
+        color="rgba(52,107,235)"
+        label="Render Duration"
+        list={list}
+        selector={selectRenderDuration}
+      />
+      <LinePlot
+        color="rgba(119,150,6)"
+        label="Total time spent"
+        list={list}
+        selector={selectTimeSpent}
+      />
+    </>
+  );
+}
+
+export type PageProfilerProps = {
+  durationMs?: number;
+};
+
 export function PageProfiler({
   children,
   durationMs = 1000,
@@ -38,13 +113,24 @@ export function PageProfiler({
   const [isProfiling, setIsProfiling] = useState(false);
   const timerRef = useRef<number | null>(null);
   const renderDurations = useRef<ProfileEvent[]>([]);
+  const profiledDataListRef = useRef<ProfiledData[]>([]);
+  const [profiledDataState, setProfiledDataState] = useState<ProfiledData[]>(
+    []
+  );
   const [profiledData, setProfiledData] =
     useState<ProfiledData>(defaultProfiledData);
 
   const handleReset = useStableCallback(() => {
+    console.log("Reset");
     timerRef.current = null;
     renderDurations.current = [];
+    profiledDataListRef.current = [];
     setProfiledData(defaultProfiledData);
+    setProfiledDataState([]);
+  });
+
+  const handleShowPlot = useStableCallback(() => {
+    setProfiledDataState([...profiledDataListRef.current]);
   });
 
   const handleProfilingButtonClick = useStableCallback(() => {
@@ -69,11 +155,15 @@ export function PageProfiler({
             ) / count
           : 0;
 
-        setProfiledData({
+        const profiledData = {
           count,
           averageRenderDuration,
           averageCommitDuation,
-        });
+        };
+        setProfiledData(profiledData);
+        if (count > 0) {
+          profiledDataListRef.current.push(profiledData);
+        }
         renderDurations.current = [];
       }, durationMs);
     }
@@ -117,13 +207,11 @@ export function PageProfiler({
           >
             {isProfiling ? "Stop Profiling" : "Start Profiling"}
           </Button>
-          <Button
-            variant="light"
-            color="primary"
-            onClick={handleReset}
-            disabled={isProfiling}
-          >
+          <Button variant="light" color="primary" onClick={handleReset}>
             Reset Profiling Data
+          </Button>
+          <Button variant="light" color="primary" onClick={handleShowPlot}>
+            Plot Graph
           </Button>
         </div>
         <p className="mt-4">
@@ -136,6 +224,9 @@ export function PageProfiler({
             <br />
             Renders: {profiledData.count}
           </Code>
+        </p>
+        <p className="pt-2">
+          <ProfilePlot list={profiledDataState} />
         </p>
       </footer>
     </article>
